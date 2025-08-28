27a6f8bbad26800d68d309797ba1b1ea8e9a272c
David Silvera <dav.silvera@gmail.com>
2025-08-28 13:10:17 +0300

    fix package

--- Package.swift
+++ Package.swift
@@ -1,20 +1,19 @@
+// swift-tools-version:5.7
 import PackageDescription
 
 let package = Package(
     name: "ChatAIKit",
-    platforms: [
-        .iOS(.v15)
-    ],
+    platforms: [.iOS(.v15)],
     products: [
         .library(
             name: "ChatAIKit",
             targets: ["ChatAIKit"]
-        ),
+        )
     ],
     targets: [
         .binaryTarget(
             name: "ChatAIKit",
-            path: "./ChatAIKit.xcframework"
-        ),
+            path: "ChatAIKit.xcframework"
+        )
     ]
 )
