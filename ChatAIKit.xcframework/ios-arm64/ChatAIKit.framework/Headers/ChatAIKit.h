753aa2cfb640203023fdd9a2d20ccdf3e110c284
David Silvera <dav.silvera@gmail.com>
2025-08-28 12:39:26 +0300

    v1

---
+++ ChatAIKit.xcframework/ios-arm64/ChatAIKit.framework/Headers/ChatAIKit.h
@@ -0,0 +1,18 @@
+//
+//  ChatAIKit.h
+//  ChatAIKit
+//
+//  Created by David on 27/08/2025.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ChatAIKit.
+FOUNDATION_EXPORT double ChatAIKitVersionNumber;
+
+//! Project version string for ChatAIKit.
+FOUNDATION_EXPORT const unsigned char ChatAIKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ChatAIKit/PublicHeader.h>
+
+
---
+++ Package.swift
@@ -0,0 +1,20 @@
+import PackageDescription
+
+let package = Package(
+    name: "ChatAIKit",
+    platforms: [
+        .iOS(.v15)
+    ],
+    products: [
+        .library(
+            name: "ChatAIKit",
+            targets: ["ChatAIKit"]
+        ),
+    ],
+    targets: [
+        .binaryTarget(
+            name: "ChatAIKit",
+            path: "./ChatAIKit.xcframework"
+        ),
+    ]
+)
